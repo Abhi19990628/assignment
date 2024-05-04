13e2eb5d91a9a87265393f0dd5ab86e09d4b0108
abhihek kumar <abhbiv5976@gmail.com>
2024-05-04 16:44:57 +0530

    api/restapi/fully working

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from .views import BankList, BranchList,BranchDetail
 
 urlpatterns = [
-    path('banks/', views.BankList.as_view(), name='bank-list'),
-    path('branches/<str:bank_name>/', views.BranchList.as_view(), name='branch-list'),
+    path('banks/', BankList.as_view(), name='bank-list'),
+    path('branches/', BranchList.as_view(), name='branch-list'),
+    path('branches/<str:ifsc>/', BranchDetail.as_view(), name='branch-detail'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Bank, Branch
 from .serializers import BankSerializer, BranchSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-class BankList(generics.ListCreateAPIView):
+
+class BankList(generics.ListAPIView):
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
 
-class BranchList(generics.ListCreateAPIView):
+class BranchList(generics.ListAPIView):
     serializer_class = BranchSerializer
 
     def get_queryset(self):
-        bank_name = self.kwargs['bank_name'] 
-        return Branch.objects.filter(bank__name__icontains=bank_name)
-    
+        ifsc = self.request.query_params.get('ifsc')
+        if ifsc:
+            return Branch.objects.filter(ifsc=ifsc)
+        else:
+            return Branch.objects.all()
+        
+class BranchDetail(generics.RetrieveAPIView):
+    queryset = Branch.objects.all()
+    serializer_class = BranchSerializer
+    lookup_field = 'ifsc'
