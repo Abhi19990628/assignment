f3eed4741fadd65cb4c8f449826742395f833487
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-05-08 03:09:43 +0530

    Update urls.py

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import BankList, BranchList,BranchDetail
+
+
+
 urlpatterns = [
     path('banks/', BankList.as_view(), name='bank-list'),
     path('branches/', BranchList.as_view(), name='branch-list'),
